5b5007ee9544c8f2e7aa20a9dfcd2ac973d8a3f6
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2016-04-13 10:33:10 -0400

    Better detect URL stuff

--- _scripts/app.js
+++ _scripts/app.js
@@ -18,6 +18,12 @@ function runApp() {
         url = url.replace("https://", "");
         url = url.replace("http://",  "");
         url = url.replace("www.",     "");
+        url = url.replace(".html",    "");
+        url = url.replace(".htm",     "");
+        url = url.replace(".php",     "");
+        url = url.replace(".aspx",    "");
+        url = url.replace(".asp",     "");
+        url = url.replace(".cfm",     "");
         url = url.split(".").join(" ");
         url = url.split("/").join(" ");
         url = url.split("?").join(" ");
