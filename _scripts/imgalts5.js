4c724961d76ec3d413c9bffca07f083a53820242
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2017-01-04 18:05:11 -0500

    Image Alts 5

---
+++ _scripts/imgalts5.js
@@ -0,0 +1,29 @@
+// Image Alts Script v5
+
+console.clear();
+
+var img = document.getElementsByTagName('img');
+var imgs = '[';
+
+for (var i = 0, image; i < img.length; i++) {
+    image = '{\"src\":\"' + img[i].src +'\",\"alt\":\"' + img[i].alt + '\"}';
+    if (i !== img.length - 1) {
+        image = image + ',';
+    }
+    imgs = imgs + image;
+}
+
+imgs = imgs + ']';
+
+console.log(imgs);
+var dummy = document.createElement('textarea');
+dummy.setAttribute('id', 'dummy');
+document.body.appendChild(dummy);
+var dumNode = document.getElementById('dummy');
+dumNode.value = imgs;
+dumNode.select();
+document.execCommand('copy');
+document.body.removeChild(dumNode);
+console.log('The above code has been copied to your clipboard');
+
+// JSON.parse(imgs); To convert it to something useful
