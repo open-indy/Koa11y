08839d340c40753a2a7d3ffeaede0bf1e1d61db8
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2017-01-15 17:02:59 -0500

    experiment

--- _scripts/app.js
+++ _scripts/app.js
@@ -295,8 +295,11 @@ function runApp () {
     }
 
     function loadImagesInModal () {
+        $('#imageAltsDonut').fadeOut('fast');
+        debugger;
         var data = JSON.parse($('#imagealts').val());
-data = window.x;
+        $('#imageAltsThumbs').empty();
+
         fs.readdir(temp, function (err, files) {
             if (err) {
                 errorMessage(err);
@@ -309,61 +312,28 @@ data = window.x;
                 var alt = data[filename].alt;
                 var src = path.join(temp, file);
                 var image =
-                  '<div>' +
-                    '<img src="' + src + '">' +
-                    '<span>' + (i + 1) + '</span>' +
+                  '<div data-imgnum="' + i + '">' +
+                    '<figure>' +
+                      '<img src="' + src + '">' +
+                      '<figcaption>' + alt + '</figcaption>' +
+                    '</figure>' +
+                    '<button class="btn btn-success">Yes</button>' +
+                    '<button class="btn btn-danger">No</button>' +
                   '</div>';
-                console.log(image);
+                $('#imageAltsThumbs').append(image);
             });
         });
     }
 
-window.x = [
-    { 'src': 'http://scout-app.io/_img/scout-app-logo.svg', 'alt': 'Scout-App 2 Logo featuring Scout the Puppy.' },
-    { 'src': 'http://scout-app.io/_img/logo-win.svg', 'alt': 'Windows' },
-    { 'src': 'http://scout-app.io/_img/logo-ubuntu.svg', 'alt': 'Ubuntu' },
-    { 'src': 'http://scout-app.io/_img/logo-zorin.svg', 'alt': 'Zorin' },
-    { 'src': 'http://scout-app.io/_img/logo-osx.svg', 'alt': 'OSX' },
-    { 'src': 'http://scout-app.io/_img/screenshots/win/01.png', 'alt': 'First Time User Experience (Windows 7)' },
-    { 'src': 'http://scout-app.io/_img/screenshots/win/02.png', 'alt': 'Project view (Windows 7)' },
-    { 'src': 'http://scout-app.io/_img/screenshots/win/25.png', 'alt': 'Multi-Project Import (Windows 7)' },
-    { 'src': '', 'alt': 'Status of All Projects (Windows 7)' },
-    { 'src': '', 'alt': 'Dev Tools are always accessible (Windows 7)' },
-    { 'src': '', 'alt': 'Windows 10' },
-    { 'src': 'http://scout-app.io/_img/logo-win.svg', 'alt': 'Windows' },
-    { 'src': 'http://scout-app.io/_img/logo-ubuntu.svg', 'alt': 'Ubuntu' },
-    { 'src': 'http://scout-app.io/_img/logo-zorin.svg', 'alt': 'Zorin' },
-    { 'src': 'http://scout-app.io/_img/logo-osx.svg', 'alt': 'OSX' },
-    { 'src': 'http://scout-app.io/_img/screenshots/win/02.png', 'alt': 'English (Windows 7)' },
-    { 'src': 'http://scout-app.io/_img/screenshots/win/03.png', 'alt': 'Dutch (Windows 7)' },
-    { 'src': 'http://scout-app.io/_img/screenshots/win/04.png', 'alt': 'French (Windows 7)' },
-    { 'src': '', 'alt': 'Russian (Windows 7)' },
-    { 'src': 'http://scout-app.io/_img/logo-win.svg', 'alt': 'Windows' },
-    { 'src': 'http://scout-app.io/_img/logo-ubuntu.svg', 'alt': 'Ubuntu' },
-    { 'src': 'http://scout-app.io/_img/logo-zorin.svg', 'alt': 'Zorin' },
-    { 'src': 'http://scout-app.io/_img/logo-osx.svg', 'alt': 'OSX' },
-    { 'src': 'http://scout-app.io/_img/screenshots/win/02.png', 'alt': 'Simplex (Windows 7)' },
-    { 'src': 'http://scout-app.io/_img/screenshots/win/06.png', 'alt': 'Cerulean (Windows 7)' },
-    { 'src': 'http://scout-app.io/_img/screenshots/win/07.png', 'alt': 'Classic (Windows 7)' },
-    { 'src': '', 'alt': 'Cosmo (Windows 7)' },
-    { 'src': '', 'alt': 'W3suli.com (Windows 7)' },
-    { 'src': '', 'alt': 'Yeti (Windows 7)' },
-    { 'src': 'http://scout-app.io/_img/logo-nodesass.svg', 'alt': 'The Node-Sass engine.' },
-    { 'src': 'http://scout-app.io/_img/logo-ugui.png', 'alt': 'The Universal GUI Library/Framework' },
-    { 'src': 'http://scout-app.io/_img/logo-nwjs.png', 'alt': 'The NW.js Cross-Platform Runtime Environment' },
-    { 'src': 'http://scout-app.io/_img/devswag.png', 'alt': 'DevSwag Logo' },
-    { 'src': 'http://scout-app.io/_img/big-sticker.png', 'alt': 'Scout-App logo as a sticker' },
-    { 'src': 'data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7', 'alt': 'Data URI Test' }
-];
-
-    loadImagesInModal();
+    window.pickle = loadImagesInModal;
 
     $('#run').click(function (evt) {
         evt.preventDefault();
-
+debugger;
         var imgAltsVal = $('#imagealts').val();
         if (imgAltsVal) {
             $('#imageAltsModal').fadeIn('slow');
+            $('#imageAltsDonut').fadeIn('fast');
             processAltsScript(imgAltsVal, loadImagesInModal);
         } else {
             // runPa11y();
