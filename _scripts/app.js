d2d7636a4692078e79cf4d8adc9f7a3f946423fc
robgaston1 <robcoach0221@gmail.com>
2017-05-02 17:46:24 -0400

    fix the copy keyboard shortcut to command + C

--- _scripts/app.js
+++ _scripts/app.js
@@ -69,8 +69,10 @@ function runApp () {
 
     function keyBindings () {
         document.onkeydown = function (pressed) {
-        // Check CMD+Shift+R and cut
-            if (pressed.metaKey && pressed.keyCode === 82) {
+        // Check CMD+Shift+X and cut
+        // Check CMD+Shift+V and paste
+        // Check CMD+Shift+C and copy
+            if (pressed.metaKey && pressed.keyCode === 88) {
                 document.execCommand('cut');
                 return false;
             } else if (pressed.metaKey && pressed.keyCode === 67) {
