4adcae3f38ee4451211aafdcbce36b4213149fa1
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2017-11-09 19:00:38 -0500

    gutted the app

---
+++ _scripts/_components/uk-navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-left">
+      <ul class="uk-navbar-nav">
+
+        <li v-for="item in items" class="uk-active">
+          <a href="#" @hover="item.showSub = true">{{ item.name }}</a>
+          <div v-if="item.sub" class="uk-navbar-dropdown">
+            <ul class="uk-nav uk-navbar-dropdown-nav">
+
+              <li v-for="subItem in item.sub" class="uk-active">
+                <a href="#">{{ subItem.name }}</a>
+              </li>
+
+            </ul>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      items: [
+        {
+          name: 'File',
+          showSub: false,
+          sub: [
+            {
+              name: 'New'
+            },
+            {
+              name: 'Exit'
+            }
+          ]
+        }
+      ];
+    };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style>
+
+</style>
