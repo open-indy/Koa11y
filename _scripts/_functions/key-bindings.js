3aeb55fddd7dacdf95d22f0e3311360b018d7f4e
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2019-01-20 10:34:24 -0500

    Update key-bindings.js

--- _scripts/_functions/key-bindings.js
+++ _scripts/_functions/key-bindings.js
@@ -1,19 +1,23 @@
 function keyBindings () {
-    document.onkeydown = function (pressed) {
-    // Check CMD+Shift+X and cut
-    // Check CMD+Shift+V and paste
-    // Check CMD+Shift+C and copy
-        if (pressed.metaKey && pressed.keyCode === 88) {
-            document.execCommand('cut');
-            return false;
-        } else if (pressed.metaKey && pressed.keyCode === 67) {
-            document.execCommand('copy');
-            return false;
-        } else if (pressed.metaKey && pressed.keyCode === 86) {
-            document.execCommand('paste');
-            return false;
-        }
-    };
+    if (process.platform === 'darwin') {
+        document.onkeydown = function (pressed) {
+            let command = pressed.metaKey;
+            let keyC = pressed.keyCode === 67;
+            let keyV = pressed.keyCode === 86;
+            let keyX = pressed.keyCode === 88;
+
+            if (command && keyX) {
+                document.execCommand('cut');
+                return false;
+            } else if (command && keyC) {
+                document.execCommand('copy');
+                return false;
+            } else if (command && keyV) {
+                document.execCommand('paste');
+                return false;
+            }
+        };
+    }
 }
 
 module.exports = keyBindings;
